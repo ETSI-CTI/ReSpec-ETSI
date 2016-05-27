400e6c3a0213e2aba98b18da3ed15ad87907dbda
Denis Filatov <filatov.dv.conseil@gmail.com>
2016-05-27 11:35:18 +0200

    mark NO style as note

--- js/etsi/etsi-style.js
+++ js/etsi/etsi-style.js
@@ -7,6 +7,7 @@ define(
         run: function(conf, doc, cb, msg) {
           msg.pub("start", "etsi/etsi-style");
           utils.linkCSS(doc, conf.rootHtml + "/etsi.css");
+          $("p.NO").addClass('note');
           msg.pub("end", "etsi/etsi-style");
           cb();
         }
